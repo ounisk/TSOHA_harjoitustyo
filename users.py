80ba1baee3629733799e9afbaf45c8b39bd51cde
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-04-04 13:50:15 +0300

    Add comment for login-function

---
+++ users.py
@@ -0,0 +1,38 @@
+from db import db
+from flask import session
+import secrets
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+def login(username, password):
+    sql = "SELECT id, password FROM allusers WHERE username=:username"   # select also admin?? comm. su 3.4.2022
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if not user:
+        return False
+    else:
+        if check_password_hash(user.password, password):
+            session["user_id"]=user.id
+            session["username"]=username
+            session["csrf_token"] = secrets.token_hex(16) #CSRF, see: part 4, insert into the form as well?
+            return True
+        else:
+            return False
+
+def logout():
+    del session["username"]
+    del session["user_id"]
+
+
+def register(username, password):
+    hash_value = generate_password_hash(password)
+    try:
+        sql = "INSERT INTO allusers (username, password) VALUES (:username, :password)"
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        db.session.commit()
+    except:
+        return False    # if username in use, error
+    return login(username, password)
+
+def user_id():
+    return session.get("user_id",0)
