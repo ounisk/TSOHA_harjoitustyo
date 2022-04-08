202de822745dddc7308c484723bfac24fbc5858c
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-04-08 13:14:22 +0300

    Correct thread to threads in get_path

--- messages.py
+++ messages.py
@@ -44,7 +44,7 @@ def get_messages(thread_id): #8.4
 
 def get_path(topic_id, thread_id): #8.4
     sql="SELECT topics.topic, threads.thread FROM topics, threads "\
-        "WHERE topics.id=:topic_id AND thread.id=:thread_id"     
+        "WHERE topics.id=:topic_id AND threads.id=:thread_id"     
     result=db.session.execute(sql, {"topic_id":topic_id, "thread_id":thread_id})
     return result.fetchone()      
 
