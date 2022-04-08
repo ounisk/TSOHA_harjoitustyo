8d2c65622e3a2af0c44107b32a98dc8ebbabdd43
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-04-08 14:04:30 +0300

    Modify database_url

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 
-app.config["SQLALCHEMY_DATABASE_URI"]="postgresql+psycopg2://"
+
+#app.config["SQLALCHEMY_DATABASE_URI"]="postgresql+psycopg2://"
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-#app.secret_key=getenv("SECRET_KEY")   # is this needed here
