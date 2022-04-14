9205ce1980376b7ba62b082da719da5c43172fdd
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-04-14 19:59:26 +0300

    Add queries to check admin, get user_id & grant access

--- users.py
+++ users.py
@@ -5,15 +5,18 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 
 def login(username, password):
-    sql = "SELECT id, password FROM allusers WHERE username=:username"   # select also admin?? comm. su 3.4.2022
+    #sql = "SELECT id, password FROM allusers WHERE username=:username"   # select also admin?? comm. su 3.4.2022
+    sql = "SELECT id, password, admin FROM allusers WHERE username=:username" #corrected 11.4 with admin
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if not user:
         return False
     else:
         if check_password_hash(user.password, password):
-            session["user_id"]=user.id
-            session["username"]=username
+            session["user_id"] = user.id
+            session["username"] = username
+            session["admin"] = user.admin  #added 11.4
+            #print("Onko user admin?", session["admin"])
             session["csrf_token"] = secrets.token_hex(16) #CSRF, see: part 4, insert into the form as well?
             return True
         else:
@@ -22,6 +25,7 @@ def login(username, password):
 def logout():
     del session["username"]
     del session["user_id"]
+    del session["admin"]  #added 11.4
 
 
 def register(username, password):
@@ -35,4 +39,21 @@ def register(username, password):
     return login(username, password)
 
 def user_id():
-    return session.get("user_id",0)
+    return session.get("user_id", 0)
+
+def is_admin():   #14.4
+    return session.get("admin", 0)    
+
+def get_user_id(username):   #13.4
+    sql="SELECT id FROM allusers WHERE username=:username"    
+    result=db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if user:
+        return user.id   # vai user[0]
+    else:
+        return False  
+
+def grant_access(topic_id, user_id): #13.4
+    sql="INSERT INTO topics_private (topic_id, user_id) VALUES (:topic_id, :user_id)"
+    db.session.execute(sql, {"topic_id":topic_id, "user_id":user_id})      
+    db.session.commit()  
