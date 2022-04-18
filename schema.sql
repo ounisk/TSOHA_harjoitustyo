637a8761446147c8ef3f653780b7b3f211c4f935
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-04-18 17:51:51 +0300

    Modify tables with ON DELETE CASCADE -property

--- schema.sql
+++ schema.sql
@@ -12,18 +12,18 @@ CREATE TABLE topics (
 CREATE TABLE threads (
     id SERIAL PRIMARY KEY,
     thread TEXT,
-    topic_id INTEGER REFERENCES topics,
-    user_id INTEGER REFERENCES allusers
+    topic_id INTEGER REFERENCES topics ON DELETE CASCADE,
+    user_id INTEGER REFERENCES allusers ON DELETE CASCADE
 );
 CREATE TABLE allmessages (
     id SERIAL PRIMARY KEY,
     content TEXT,
-    thread_id INTEGER REFERENCES threads,
-    user_id INTEGER REFERENCES allusers,
+    thread_id INTEGER REFERENCES threads ON DELETE CASCADE,
+    user_id INTEGER REFERENCES allusers ON DELETE CASCADE,
     sent_at TIMESTAMP
 );
 CREATE TABLE topics_private (
     id SERIAL PRIMARY KEY,
-    topic_id INTEGER REFERENCES topics,
-    user_id INTEGER REFERENCES allusers
+    topic_id INTEGER REFERENCES topics ON DELETE CASCADE,
+    user_id INTEGER REFERENCES allusers ON DELETE CASCADE
 );
