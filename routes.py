28e39320a4eff11c58572aa555b99694211abb59
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-04-22 09:19:53 +0300

    Add routes for hiding/"deleting" topics

--- routes.py
+++ routes.py
@@ -68,12 +68,9 @@ def register():
         else:
             return render_template("error.html", message="Rekisteröinti ei onnistunut, valitse toinen käyttäjätunnus")
 
-
-
 @app.route("/create_new_topic") #11.4, 
 def create_new_topic():
     return render_template("new_topic_template.html")
-
 
 @app.route("/new_topic", methods=["POST"])   #11.4, 12.4
 def new_topic():
@@ -86,14 +83,23 @@ def new_topic():
 
     topic_name = request.form["topic_name"]
     visible = request.form.get("visible")
-    #print("visibility", visibility)
 
     if len(topic_name) < 1 or len(topic_name) > 100:
         return render_template("error.html", message="Aiheen tulee olla 1-100 merkkiä")
 
-    #print("mitä on visiblessa?", visible)
     messages.new_topic(topic_name, visible)
     return redirect("/")
+
+
+@app.route("/hide_topic/<int:topic_id>", methods=["GET"])   #20.4
+def hide_topic(topic_id):    
+    messages.hide_topic(topic_id)
+    return redirect("/") 
+
+@app.route("/hide_secret_topic/<int:topic_id>", methods=["GET"])   #20.4
+def hide_secret_topic(topic_id):    
+    messages.hide_secret_topic(topic_id)
+    return redirect("/") 
 
 
 @app.route("/access/<int:topic_id>")    #13.4
@@ -110,7 +116,6 @@ def grant_topic_access():
         abort(403)
 
     topic_id = request.form["topic_id"]
-    #print("topic_id", topic_id)
     username=request.form["username"]
 
     if len(username) < 1 or len(username) > 50:
@@ -153,11 +158,10 @@ def new_thread():
     return redirect("/topic/" + str(topic_id))
 
 
-
-
 @app.route("/edit_thread/<int:topic_id>/<int:thread_id>")   #18.4
 def edit_thread(topic_id, thread_id):
-    return render_template("edit_thread.html", topic_id=topic_id, thread_id=thread_id)
+    thread_name = messages.get_thread_to_edit(thread_id)
+    return render_template("edit_thread.html", topic_id=topic_id, thread_id=thread_id, thread_name=thread_name)
 
 
 @app.route("/edit_thread_header", methods=["POST"])    #18.4
@@ -179,8 +183,7 @@ def edit_thread_header():
 @app.route("/delete_thread/<int:thread_id>", methods=["GET"])   #18.4
 def delete_thread(thread_id):
     #if session["csrf_token"] != request.form["csrf_token"]:    # to take into acc. CSRF-vulnerability
-    #    abort(403)   #not working???
-    #thread_id = request.form["thread_id"]    
+    #    abort(403)   #check if needed??  
     topic_id = messages.delete_this_thread(thread_id)[0]
 
     return redirect("/topic/" + str(topic_id))
@@ -217,11 +220,10 @@ def new_message():
         return render_template("error.html", message="Viestin lähetys ei onnistunut.")
 
 
-
-
-@app.route("/edit_message/<int:topic_id>/<int:thread_id>/<int:message_id>")   #19.4
+@app.route("/edit_message/<int:topic_id>/<int:thread_id>/<int:message_id>")   #19.4 & 21.4
 def edit_message(topic_id, thread_id, message_id): # include old content
-    return render_template("edit_message.html", topic_id=topic_id, thread_id=thread_id, message_id=message_id)
+    message = messages.get_message_to_edit(message_id) 
+    return render_template("edit_message.html", topic_id=topic_id, thread_id=thread_id, message_id=message_id, message=message)
 
 
 @app.route("/edit_message_text", methods=["POST"])   #19.4
@@ -246,8 +248,7 @@ def edit_message_text():
 @app.route("/delete_message/<int:topic_id>/<int:thread_id>/<int:message_id>", methods=["GET"])   #19.4
 def delete_message(topic_id, thread_id, message_id):
     #if session["csrf_token"] != request.form["csrf_token"]:    # to take into acc. CSRF-vulnerability
-    #    abort(403)   #not working???
-    #thread_id = request.form["thread_id"]    
+    #    abort(403)   #check if needed  
     thread_id = messages.delete_this_message(message_id)[0]
 
     return redirect("/topic/" + str(topic_id) + "/thread/" + str(thread_id))
@@ -263,7 +264,7 @@ def search_result():
     if len(query) < 1 or len(query) > 50:
         return render_template("error.html", message = "Haettavan sanan pituus voi olla 1-50 merkkiä.")
     result_messages = messages.search_message(query)
-    #print("result_messages", result_messages)
+
     if result_messages is None or len(result_messages) == 0:
         return render_template("error.html", message = "Hakusanalla ei löydy viestejä.")
 
