bc3b50b4ee1760c2640c1836482bb2b45adef752
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-04-23 11:31:06 +0300

    Make pylint correction suggestions

--- db.py
+++ db.py
@@ -1,7 +1,6 @@
-from app import app
-from flask_sqlalchemy import SQLAlchemy
 from os import getenv
-
+from flask_sqlalchemy import SQLAlchemy
+from app import app
 
 
 #app.config["SQLALCHEMY_DATABASE_URI"]="postgresql+psycopg2://"
