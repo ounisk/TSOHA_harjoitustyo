994e0a83b951f8d7d47935b4cd3a78a3f306e00c
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-04-23 11:34:23 +0300

    Add functions to check access incl. edit access

--- users.py
+++ users.py
@@ -1,7 +1,8 @@
-from db import db
-from flask import session
 import secrets
+from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
+from db import db
+
 
 
 def login(username, password):
@@ -16,7 +17,6 @@ def login(username, password):
             session["user_id"] = user.id
             session["username"] = username
             session["admin"] = user.admin  #added 11.4
-            #print("Onko user admin?", session["admin"])
             session["csrf_token"] = secrets.token_hex(16) #CSRF, see: part 4, insert into the form as well?
             return True
         else:
@@ -42,18 +42,50 @@ def user_id():
     return session.get("user_id", 0)
 
 def is_admin():   #14.4
-    return session.get("admin", 0)    
+    return session.get("admin", 0)
 
 def get_user_id(username):   #13.4
-    sql="SELECT id FROM allusers WHERE username=:username"    
-    result=db.session.execute(sql, {"username":username})
+    sql = "SELECT id FROM allusers WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if user:
-        return user.id   # vai user[0]
+        return user.id
     else:
-        return False  
+        return False
 
 def grant_access(topic_id, user_id): #13.4
-    sql="INSERT INTO topics_private (topic_id, user_id) VALUES (:topic_id, :user_id)"
-    db.session.execute(sql, {"topic_id":topic_id, "user_id":user_id})      
-    db.session.commit()  
+    sql = "INSERT INTO topics_private (topic_id, user_id) VALUES (:topic_id, :user_id)"
+    db.session.execute(sql, {"topic_id":topic_id, "user_id":user_id})
+    db.session.commit()
+
+def has_access(topic_id):   #22.4 - unauthorized can't access secret pages
+    user_id = session.get("user_id", 0)
+    is_admin = session.get("admin", 0)
+    if is_admin == 0:
+        is_admin = False
+    sql = "SELECT topics.id FROM topics WHERE topics.id=:topic_id "\
+        "AND (topics.visible=TRUE OR :is_admin OR :user_id IN "\
+        "(SELECT topics_private.user_id FROM topics_private "\
+        "WHERE topics_private.topic_id=topics.id))"
+    result = db.session.execute(sql, {"topic_id":topic_id, "is_admin":is_admin, "user_id":user_id})
+    return result.fetchone()
+
+def thread_edit_access(thread_id):   #23.4 - unauthorized can't access secret pages
+    user_id = session.get("user_id", 0)
+    is_admin = session.get("admin", 0)
+    if is_admin == 0:
+        is_admin = False
+    sql = "SELECT id FROM threads WHERE id=:thread_id "\
+        "AND (user_id=:user_id OR :is_admin)"
+    result = db.session.execute(sql, {"thread_id":thread_id, "is_admin":is_admin, "user_id":user_id})
+    return result.fetchone()
+
+def message_edit_access(message_id):   #23.4 - unauthorized can't access secret pages
+    user_id = session.get("user_id", 0)
+    is_admin = session.get("admin", 0)
+    if is_admin == 0:
+        is_admin = False
+    sql = "SELECT id FROM allmessages WHERE id=:message_id "\
+        "AND (user_id=:user_id OR :is_admin)"
+    result = db.session.execute(sql, {"message_id":message_id, "is_admin":is_admin, "user_id":user_id})
+    return result.fetchone()
