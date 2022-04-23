fb4293cd824ab2fc47446492ef17c2036179a5ea
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-04-23 11:30:44 +0300

    Make pylint correction suggestions

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
 from os import getenv
+from flask import Flask
 
 app = Flask(__name__)
-app.secret_key=getenv("SECRET_KEY")   
+app.secret_key = getenv("SECRET_KEY")   
 
 import routes
 import users
