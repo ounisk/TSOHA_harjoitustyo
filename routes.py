e1b6f7348a0fae1855153a68c4c78b8d1a9611ce
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-05-04 20:14:20 +0300

    Delete comments & include errors on relevant templates

--- routes.py
+++ routes.py
@@ -7,12 +7,11 @@ import users, messages
 @app.route("/")
 def index():
     list = messages.get_list()
-    return render_template("index.html", topics=list)    # add: no. of threads, no. of messages and time of last msg
+    return render_template("index.html", topics=list)
 
-
-@app.route("/topic/<int:topic_id>")  # 7.4
+@app.route("/topic/<int:topic_id>")
 def topic(topic_id):
-    if users.has_access(topic_id) is None: # 22.4
+    if users.has_access(topic_id) is None:
         return render_template("error.html", message="Käyttäjällä ei oikeutta sivuun")
 
     list = messages.get_threads(topic_id)
@@ -22,7 +21,7 @@ def topic(topic_id):
 
 @app.route("/topic/<int:topic_id>/thread/<int:thread_id>")
 def thread(topic_id, thread_id):
-    if users.has_access(topic_id) is None: # 22.4
+    if users.has_access(topic_id) is None:
         return render_template("error.html", message="Käyttäjällä ei oikeutta sivuun")
 
     list = messages.get_messages(thread_id)
@@ -32,17 +31,18 @@ def thread(topic_id, thread_id):
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    error = None
     if request.method == "GET":
         return render_template("login.html")
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
         if users.login(username, password):
-            #print("printtausta jos löytyy")
             return redirect("/")
         else:
-            #print("printtaus jos ei löydy login")
-            return render_template("error.html", message="Käyttäjätunnus tai salasana on väärin")
+            error = "Käyttäjätunnus tai salasana on väärin."
+            #return render_template("error.html", message="Käyttäjätunnus tai salasana on väärin")
+            return render_template("login.html", error=error)
 
 
 @app.route("/logout")
@@ -74,17 +74,17 @@ def register():
         else:
             return render_template("error.html", message="Rekisteröinti ei onnistunut, valitse toinen käyttäjätunnus")
 
-@app.route("/create_new_topic") #11.4,
+@app.route("/create_new_topic")
 def create_new_topic():
     return render_template("new_topic_template.html")
 
-@app.route("/new_topic", methods=["POST"])   #11.4, 12.4
+@app.route("/new_topic", methods=["POST"])
 def new_topic():
 
-    if not session.get("admin", 0):   #if not admin, can't create new topics
+    if not session.get("admin", 0):
         abort(403)
 
-    if session["csrf_token"] != request.form["csrf_token"]:    # to take into acc. CSRF-vulnerability
+    if session["csrf_token"] != request.form["csrf_token"]:
         abort(403)
 
     topic_name = request.form["topic_name"]
@@ -98,42 +98,42 @@ def new_topic():
 
 
 
-@app.route("/hide_topic/<int:topic_id>", methods=["GET"])   #20.4 + 22.4
+@app.route("/hide_topic/<int:topic_id>", methods=["GET"])
 def hide_topic(topic_id):
-    if not session.get("admin", 0):   #if not admin, can't hide topics
+    if not session.get("admin", 0):
         abort(403)
 
     messages.hide_topic(topic_id)
     return redirect("/")
 
 
-@app.route("/hide_secret_topic/<int:topic_id>", methods=["GET"])   #20.4 +22.4
+@app.route("/hide_secret_topic/<int:topic_id>", methods=["GET"])
 def hide_secret_topic(topic_id):
-    if not session.get("admin", 0):   #if not admin, can't hide topics
+    if not session.get("admin", 0):
         abort(403)
 
     messages.hide_secret_topic(topic_id)
     return redirect("/")
 
 
-@app.route("/access/<int:topic_id>")    #13.4
+@app.route("/access/<int:topic_id>")
 def access(topic_id):
     return render_template("access.html", topic_id=topic_id)
 
 
-@app.route("/grant_topic_access", methods=["POST"])    #13.4
+@app.route("/grant_topic_access", methods=["POST"])
 def grant_topic_access():
-    if not session.get("admin", 0):   #if not admin, can't give access
+    if not session.get("admin", 0):
         abort(403)
 
-    if session["csrf_token"] != request.form["csrf_token"]:    # to take into acc. CSRF-vulnerability
+    if session["csrf_token"] != request.form["csrf_token"]:
         abort(403)
 
     topic_id = request.form["topic_id"]
     username = request.form["username"]
 
     if len(username) < 1 or len(username) > 50:
-            return render_template("error.html", message="Käyttäjätunnuksen pituus on 1-50 merkkiä")
+        return render_template("error.html", message="Käyttäjätunnuksen pituus on 1-50 merkkiä")
 
     user_id = users.get_user_id(username)
 
@@ -144,19 +144,19 @@ def grant_topic_access():
     return redirect("/")
 
 
-@app.route("/new_thread_template/<int:topic_id>")   #9.4
+@app.route("/new_thread_template/<int:topic_id>")
 def new(topic_id):
     return render_template("new_thread_template.html", topic_id=topic_id)
 
 
-@app.route("/new_thread", methods=["POST"])   #9.4
+@app.route("/new_thread", methods=["POST"])
 def new_thread():
     user_id = users.user_id()
 
-    if user_id == 0:   # 22.4 has to be signed-in
+    if user_id == 0:
         return render_template("error.html", message="Et ole kirjautunut sisään.")
 
-    if session["csrf_token"] != request.form["csrf_token"]:    # to take into acc. CSRF-vulnerability
+    if session["csrf_token"] != request.form["csrf_token"]:
         abort(403)
 
     thread_name = request.form["thread_name"]
@@ -175,26 +175,26 @@ def new_thread():
     return redirect("/topic/" + str(topic_id))
 
 
-@app.route("/edit_thread/<int:topic_id>/<int:thread_id>")   #18.4
+@app.route("/edit_thread/<int:topic_id>/<int:thread_id>")
 def edit_thread(topic_id, thread_id):
     thread_name = messages.get_thread_to_edit(thread_id)
     return render_template("edit_thread.html", topic_id=topic_id, thread_id=thread_id, thread_name=thread_name)
 
 
-@app.route("/edit_thread_header", methods=["POST"])    #18.4
+@app.route("/edit_thread_header", methods=["POST"])
 def edit_thread_header():
     user_id = users.user_id()
 
-    if user_id == 0:   # 22.4 has to be signed-in
+    if user_id == 0:
         return render_template("error.html", message="Et ole kirjautunut sisään.")
 
-    if session["csrf_token"] != request.form["csrf_token"]:    # to take into acc. CSRF-vulnerability
+    if session["csrf_token"] != request.form["csrf_token"]:
         abort(403)
     thread_name = request.form["thread_name"]
     thread_id = request.form["thread_id"]
     topic_id = request.form["topic_id"]
 
-    if users.thread_edit_access(thread_id) is None: # 23.4 only if own or admin
+    if users.thread_edit_access(thread_id) is None:
         return render_template("error.html", message="Käyttäjällä ei oikeutta muokata ketjua.")
 
     if len(thread_name) < 1 or len(thread_name) > 100:
@@ -205,16 +205,14 @@ def edit_thread_header():
     return redirect("/topic/" + str(topic_id))
 
 
-@app.route("/delete_thread/<int:thread_id>", methods=["GET"])   #18.4
+@app.route("/delete_thread/<int:thread_id>", methods=["GET"])
 def delete_thread(thread_id):
-    #if session["csrf_token"] != request.form["csrf_token"]:    # to take into acc. CSRF-vulnerability
-    #    abort(403)   #check if needed??
     user_id = users.user_id()
 
-    if user_id == 0:   # 22.4 has to be signed-in
+    if user_id == 0:
         return render_template("error.html", message="Et ole kirjautunut sisään.")
 
-    if users.thread_edit_access(thread_id) is None: # 23.4 only if own or admin
+    if users.thread_edit_access(thread_id) is None:
         return render_template("error.html", message="Käyttäjällä ei oikeutta muokata ketjua.")
 
     topic_id = messages.delete_this_thread(thread_id)[0]
@@ -224,18 +222,20 @@ def delete_thread(thread_id):
 
 
 @app.route("/message/<int:topic_id>/<int:thread_id>")
-def message(topic_id, thread_id):     #fixed 9.4,
-    return render_template("message.html", topic_id=topic_id, thread_id=thread_id)   #  add topic 4.4
+def message(topic_id, thread_id):
+    return render_template("message.html", topic_id=topic_id, thread_id=thread_id)
 
 
-@app.route("/new_message", methods=["POST"])   # modified 9.4
+@app.route("/new_message", methods=["POST"])
 def new_message():
+    error = None
+
     user_id = users.user_id()
 
-    if user_id == 0:   # 22.4 has to be signed-in
+    if user_id == 0:
         return render_template("error.html", message="Et ole kirjautunut sisään.")
 
-    if session["csrf_token"] != request.form["csrf_token"]:    # to take into acc. CSRF-vulnerability
+    if session["csrf_token"] != request.form["csrf_token"]:
         abort(403)
 
     message = request.form["message"]
@@ -243,46 +243,53 @@ def new_message():
     thread_id = request.form["thread_id"]
 
     if len(message) > 5000:
-        return render_template("error.html", message="Viesti on liian pitkä.") #
+        error = "Viesti on liian pitkä."
+        return render_template("message.html", error=error, topic_id=topic_id, thread_id=thread_id) # 4.5 että toimii virheessä oikein
 
     if len(message) < 1:
-        return render_template("error.html", message="Viesti on liian lyhyt.") #
+        error = "Viesti on liian lyhyt."
+        return render_template("message.html", error=error, topic_id=topic_id, thread_id=thread_id) # 4.5
 
     if messages.send(message, thread_id, user_id):
-        #return redirect("/")   # or return redirect("/topic"+ topic_id)  ??
         return redirect("/topic/" + str(topic_id) + "/thread/" + str(thread_id))
     else:
         return render_template("error.html", message="Viestin lähetys ei onnistunut.")
 
 
-@app.route("/edit_message/<int:topic_id>/<int:thread_id>/<int:message_id>")   #19.4 & 21.4
-def edit_message(topic_id, thread_id, message_id): # include old content
+@app.route("/edit_message/<int:topic_id>/<int:thread_id>/<int:message_id>")
+def edit_message(topic_id, thread_id, message_id):
     message = messages.get_message_to_edit(message_id)
     return render_template("edit_message.html", topic_id=topic_id, thread_id=thread_id, message_id=message_id, message=message)
 
 
-@app.route("/edit_message_text", methods=["POST"])   #19.4
+@app.route("/edit_message_text", methods=["POST"])
 def edit_message_text():
     user_id = users.user_id()
 
-    if user_id == 0:   # 22.4 has to be signed-in
+    error = None
+
+    if user_id == 0:
         return render_template("error.html", message="Et ole kirjautunut sisään.")
 
-    if session["csrf_token"] != request.form["csrf_token"]:    # to take into acc. CSRF-vulnerability
+    if session["csrf_token"] != request.form["csrf_token"]:
         abort(403)
     message = request.form["message"]
     topic_id = request.form["topic_id"]
     thread_id = request.form["thread_id"]
     message_id = request.form["message_id"]
 
-    if users.message_edit_access(message_id) is None: # 23.4 only if own msg or admin
+    if users.message_edit_access(message_id) is None:
         return render_template("error.html", message="Käyttäjällä ei oikeutta muokata viestiä.")
 
     if len(message) > 5000:
-        return render_template("error.html", message="Viesti on liian pitkä.") #
+        error = "Viesti on liian pitkä."
+        return render_template("edit_message.html", error=error, topic_id=topic_id, thread_id=thread_id, message_id=message_id, message=message)
+        #return render_template("error.html", message="Viesti on liian pitkä.")
 
     if len(message) < 1:
-        return render_template("error.html", message="Viesti on liian lyhyt.") #
+        error = "Viesti on liian lyhyt."
+        return render_template("edit_message.html", error=error, topic_id=topic_id, thread_id=thread_id, message_id=message_id, message=message)
+        #return render_template("error.html", message="Viesti on liian lyhyt.")
 
     messages.edit_message(message, message_id)
     return redirect("/topic/" + str(topic_id) + "/thread/" + str(thread_id))
@@ -290,14 +297,13 @@ def edit_message_text():
 
 @app.route("/delete_message/<int:topic_id>/<int:thread_id>/<int:message_id>", methods=["GET"])   #19.4
 def delete_message(topic_id, thread_id, message_id):
-    #if session["csrf_token"] != request.form["csrf_token"]:    # to take into acc. CSRF-vulnerability
-    #    abort(403)   #check if needed
+
     user_id = users.user_id()
 
-    if user_id == 0:   # 22.4 has to be signed-in
+    if user_id == 0:
         return render_template("error.html", message="Et ole kirjautunut sisään.")
 
-    if users.message_edit_access(message_id) is None: # 23.4 only if own msg or admin
+    if users.message_edit_access(message_id) is None:
         return render_template("error.html", message="Käyttäjällä ei oikeutta poistaa viestiä.")
 
     thread_id = messages.delete_this_message(message_id)[0]
@@ -305,14 +311,14 @@ def delete_message(topic_id, thread_id, message_id):
     return redirect("/topic/" + str(topic_id) + "/thread/" + str(thread_id))
 
 
-@app.route("/search")    #16.4
+@app.route("/search")
 def search():
     user_id = users.user_id()
-    if user_id == 0:   # 22.4 has to be signed-in
+    if user_id == 0:
         return render_template("error.html", message="Voit hakea viestejä, kun olet kirjautunut sisään.")
     return render_template("search.html")
 
-@app.route("/search_result", methods=["GET"])    #16.4
+@app.route("/search_result", methods=["GET"])
 def search_result():
     query = request.args["query"]
     if len(query) < 1 or len(query) > 50:
